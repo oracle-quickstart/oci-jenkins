92a6ffded46bd14be0d622c11fae6fb14f72e62b
Umair <umsiddiq@UMSIDDIQ-LAP.oradev.oraclecorp.com>
2020-02-04 15:43:52 -0600

    exampl file update

--- examples/quick_start/loadbalancer.tf
+++ examples/quick_start/loadbalancer.tf
@@ -4,11 +4,11 @@
 
 resource "oci_load_balancer" "JenkinsLB" {
   shape          = "100Mbps"
-  compartment_id = "${var.compartment_ocid}"
+  compartment_id = var.compartment_ocid
 
   subnet_ids = [
-    "${oci_core_subnet.JenkinsLBSubnet1.id}",
-    "${oci_core_subnet.JenkinsLBSubnet2.id}",
+    oci_core_subnet.JenkinsLBSubnet1.id,
+#    oci_core_subnet.JenkinsLBSubnet2.id,
   ]
 
   display_name = "JenkinsLB"
@@ -16,19 +16,19 @@ resource "oci_load_balancer" "JenkinsLB" {
 
 resource "oci_load_balancer_backend_set" "JenkinsLBBes" {
   name             = "JenkinsLBBes"
-  load_balancer_id = "${oci_load_balancer.JenkinsLB.id}"
+  load_balancer_id = oci_load_balancer.JenkinsLB.id
   policy           = "ROUND_ROBIN"
 
   health_checker {
-    port     = "${var.http_port}"
+    port     = var.http_port
     protocol = "TCP"
   }
 }
 
 resource "oci_load_balancer_listener" "JenkinsLBLsnr" {
-  load_balancer_id         = "${oci_load_balancer.JenkinsLB.id}"
+  load_balancer_id         = oci_load_balancer.JenkinsLB.id
   name                     = "http"
-  default_backend_set_name = "${oci_load_balancer_backend_set.JenkinsLBBes.name}"
+  default_backend_set_name = oci_load_balancer_backend_set.JenkinsLBBes.name
   port                     = 80
   protocol                 = "HTTP"
 
@@ -38,10 +38,10 @@ resource "oci_load_balancer_listener" "JenkinsLBLsnr" {
 }
 
 resource "oci_load_balancer_backend" "JenkinsLBBe" {
-  load_balancer_id = "${oci_load_balancer.JenkinsLB.id}"
-  backendset_name  = "${oci_load_balancer_backend_set.JenkinsLBBes.name}"
-  ip_address       = "${module.jenkins.master_private_ip}"
-  port             = "${var.http_port}"
+  load_balancer_id = oci_load_balancer.JenkinsLB.id
+  backendset_name  = oci_load_balancer_backend_set.JenkinsLBBes.name
+  ip_address       = module.jenkins.master_private_ip
+  port             = var.http_port
   backup           = false
   drain            = false
   offline          = false
@@ -49,15 +49,15 @@ resource "oci_load_balancer_backend" "JenkinsLBBe" {
 }
 
 resource "tls_private_key" "JenkinTLS" {
-  count     = "${var.listener_ca_certificate == "" ? 1 : 0 }"
+  count     = var.listener_ca_certificate == "" ? 1 : 0
   algorithm = "RSA"
   rsa_bits  = 4096
 }
 
 resource "tls_self_signed_cert" "JenkinsCert" {
-  count           = "${var.listener_ca_certificate == "" ? 1 : 0 }"
-  key_algorithm   = "${tls_private_key.JenkinTLS.algorithm}"
-  private_key_pem = "${tls_private_key.JenkinTLS.private_key_pem}"
+  count           = var.listener_ca_certificate == "" ? 1 : 0
+  key_algorithm   = tls_private_key.JenkinTLS[0].algorithm
+  private_key_pem = tls_private_key.JenkinTLS[0].private_key_pem
 
   validity_period_hours = 26280
   early_renewal_hours   = 8760
@@ -71,11 +71,11 @@ resource "tls_self_signed_cert" "JenkinsCert" {
 }
 
 resource "oci_load_balancer_certificate" "JenkinsLBCert" {
-  load_balancer_id   = "${oci_load_balancer.JenkinsLB.id}"
-  ca_certificate     = "${var.listener_ca_certificate == "" ? "${tls_self_signed_cert.JenkinsCert.cert_pem}" : var.listener_ca_certificate}"
+  load_balancer_id   = oci_load_balancer.JenkinsLB.id
+  ca_certificate     = var.listener_ca_certificate == "" ? tls_self_signed_cert.JenkinsCert[0].cert_pem : var.listener_ca_certificate
   certificate_name   = "JenkinsCert"
-  private_key        = "${var.listener_private_key == "" ? "${tls_private_key.JenkinTLS.private_key_pem}" : var.listener_private_key}"
-  public_certificate = "${var.listener_public_certificate == "" ? "${tls_self_signed_cert.JenkinsCert.cert_pem}" : var.listener_public_certificate}"
+  private_key        = var.listener_private_key == "" ? tls_private_key.JenkinTLS[0].private_key_pem : var.listener_private_key
+  public_certificate = var.listener_public_certificate == "" ? tls_self_signed_cert.JenkinsCert[0].cert_pem : var.listener_public_certificate
 
   lifecycle {
     create_before_destroy = true
@@ -83,14 +83,15 @@ resource "oci_load_balancer_certificate" "JenkinsLBCert" {
 }
 
 resource "oci_load_balancer_listener" "JenkinsLBLsnr_SSL" {
-  load_balancer_id         = "${oci_load_balancer.JenkinsLB.id}"
+  load_balancer_id         = oci_load_balancer.JenkinsLB.id
   name                     = "https"
-  default_backend_set_name = "${oci_load_balancer_backend_set.JenkinsLBBes.name}"
+  default_backend_set_name = oci_load_balancer_backend_set.JenkinsLBBes.name
   port                     = 443
   protocol                 = "HTTP"
 
   ssl_configuration {
-    certificate_name        = "${oci_load_balancer_certificate.JenkinsLBCert.certificate_name}"
+    certificate_name        = oci_load_balancer_certificate.JenkinsLBCert.certificate_name
     verify_peer_certificate = false
   }
 }
+
--- examples/quick_start/main.tf
+++ examples/quick_start/main.tf
@@ -2,22 +2,22 @@
 # Setup Bastion Host
 # ------------------------------------------------------------------------------
 resource "oci_core_instance" "JenkinsBastion" {
-  availability_domain = "${data.template_file.ad_names.*.rendered[var.bastion_ad_index]}"
-  compartment_id      = "${var.compartment_ocid}"
-  display_name        = "${var.bastion_display_name}"
-  shape               = "${var.bastion_shape}"
+  availability_domain = data.template_file.ad_names[var.bastion_ad_index].rendered
+  compartment_id      = var.compartment_ocid
+  display_name        = var.bastion_display_name
+  shape               = var.bastion_shape
 
   create_vnic_details {
-    subnet_id        = "${oci_core_subnet.JenkinsBastion.id}"
+    subnet_id        = oci_core_subnet.JenkinsBastion.id
     assign_public_ip = true
   }
 
-  metadata {
-    ssh_authorized_keys = "${file("${var.bastion_authorized_keys}")}"
+  metadata = {
+    ssh_authorized_keys = file(var.bastion_authorized_keys)
   }
 
   source_details {
-    source_id   = "${var.image_id[var.region]}"
+    source_id   = var.image_id[var.region]
     source_type = "image"
   }
 }
@@ -26,20 +26,21 @@ resource "oci_core_instance" "JenkinsBastion" {
 # DEPLOY THE JENKINS CLUSTER
 # ------------------------------------------------------------------------------
 module "jenkins" {
-  source                = "../../"
-  compartment_ocid      = "${var.compartment_ocid}"
-  jenkins_version       = "${var.jenkins_version}"
-  jenkins_password      = "${var.jenkins_password}"
-  master_ad             = "${data.template_file.ad_names.*.rendered[0]}"
-  master_subnet_id      = "${oci_core_subnet.JenkinsMasterSubnetAD.id}"
-  master_image_id       = "${var.image_id[var.region]}"
-  slave_count           = "${var.slave_count}"
-  slave_ads             = "${data.template_file.ad_names.*.rendered}"
-  slave_subnet_ids      = "${split(",",join(",", oci_core_subnet.JenkinsSlaveSubnetAD.*.id))}"
-  slave_image_id        = "${var.image_id[var.region]}"
-  ssh_authorized_keys   = "${var.ssh_authorized_keys}"
-  ssh_private_key       = "${var.ssh_private_key}"
-  bastion_host          = "${oci_core_instance.JenkinsBastion.public_ip}"
-  bastion_user          = "${var.bastion_user}"
-  bastion_private_key   = "${var.bastion_private_key}"
+  source              = "../../"
+  compartment_ocid    = var.compartment_ocid
+  jenkins_version     = var.jenkins_version
+  jenkins_password    = var.jenkins_password
+  master_ad           = data.template_file.ad_names[0].rendered
+  master_subnet_id    = oci_core_subnet.JenkinsMasterSubnetAD.id
+  master_image_id     = var.image_id[var.region]
+  slave_count         = var.slave_count
+  slave_ads           = data.template_file.ad_names.*.rendered
+  slave_subnet_ids    = split(",", join(",", oci_core_subnet.JenkinsSlaveSubnetAD.*.id))
+  slave_image_id      = var.image_id[var.region]
+  ssh_authorized_keys = var.ssh_authorized_keys
+  ssh_private_key     = var.ssh_private_key
+  bastion_host        = oci_core_instance.JenkinsBastion.public_ip
+  bastion_user        = var.bastion_user
+  bastion_private_key = var.bastion_private_key
 }
+
