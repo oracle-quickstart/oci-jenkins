c2758d71a0fd8b7c629c99e2ffac4a3565cbf53d
lfeldman <lukasz.feldman@oracle.com>
2021-03-26 11:38:53 +0100

    fixed outputs

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,17 @@
 output "master_instance_id" {
-  value = "${module.jenkins-master.id}"
+  value = module.jenkins-master.id
 }
 
 output "master_private_ip" {
-  value = "${module.jenkins-master.private_ip}"
+  value = module.jenkins-master.private_ip
 }
 
 output "agent_instance_ids" {
-  value = "${module.jenkins-agent.ids}"
+  value = module.jenkins-agent.ids
 }
 
 output "agent_private_ips" {
-  value = "${module.jenkins-agent.private_ips}"
+  value = module.jenkins-agent.private_ips
 }
 
 output "master_login_url" {
