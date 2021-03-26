65934c781147f3c37ac74fbe19354e4086b220e1
lfeldman <lukasz.feldman@oracle.com>
2021-03-26 11:32:01 +0100

    Fixed modules/jenkins-agent/outputs.tf

--- modules/jenkins-agent/outputs.tf
+++ modules/jenkins-agent/outputs.tf
@@ -6,7 +6,7 @@ output "private_ips" {
   value = [oci_core_instance.TFJenkinsAgent.*.private_ip]
 }
 
-output "slave_host_names" {
+output "agent_host_names" {
   value = [oci_core_instance.TFJenkinsAgent.*.display_name]
 }
 
